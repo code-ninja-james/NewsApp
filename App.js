a06dc10bfd972c0744122d5cfd4e06f42763fcda
Jameson <githinjijmaeson@gmail.com>
2020-10-22 10:22:36 +0300

    card

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React,{useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import {AppLoading} from 'expo';
+
+
+import Header from './src/components/Header';
+import Card from './src/components/Card';
+
+const loadFonts=()=>{
+  return Font.loadAsync({
+    'Ubuntu':require('./assets/fonts/Ubuntu-Regular.ttf'),
+    'Ubuntu-Bold':require('./assets/fonts/Ubuntu-Bold.ttf'),
+  });
+}
 
 export default function App() {
+  const[fontLoaded,setFontLoaded]=useState(false);
+  if(!fontLoaded){
+    return(
+      <AppLoading
+      startAsync={loadFonts}
+      onFinish={()=>setFontLoaded(true)}
+      />
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <View>
+<Header/>
+<Card/>
+   </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ 
 });
